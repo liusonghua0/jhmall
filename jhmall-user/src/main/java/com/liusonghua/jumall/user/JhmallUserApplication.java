8900ca4edf478c6a07655c18ad68521e7be327ee
liusonghua0 <m15164300959@163.com>
2019-10-31 12:03:05 +0800

    新建user模块

---
+++ jhmall-user/src/main/java/com/liusonghua/jumall/user/JhmallUserApplication.java
@@ -0,0 +1,13 @@
+package com.liusonghua.jumall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhmallUserApplication.class, args);
+    }
+
+}
---
+++ jhmall-user/src/test/java/com/liusonghua/jumall/user/JhmallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liusonghua.jumall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhmallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
